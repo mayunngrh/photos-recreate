8be50e9ea1b8627735d6cefbce321087919cd726
Mayun Suryatama <mayunngrh@gmail.com>
2026-04-20 23:28:24 +0800

    init the branch mayun

---
+++ PhotosRecreate/Models/ImageModel.swift
@@ -0,0 +1,32 @@
+//
+//  ImageModel.swift
+//  test
+//
+//  Created by Alex on 20/04/26.
+//
+import SwiftUI
+
+struct ImageModel: Identifiable{
+//    let UID: UUID()
+    var name: String
+    var isSelected: Bool = false
+    let id: UUID = UUID()
+    
+    static var imageSet:[ImageModel] = [
+        ImageModel(name: "photo_10"),
+        ImageModel(name: "photo_11"),
+        ImageModel(name: "photo_12"),
+        ImageModel(name: "photo_13"),
+        ImageModel(name: "photo_14"),
+        ImageModel(name: "photo_16"),
+        ImageModel(name: "photo_17"),
+        ImageModel(name: "photo_18"),
+        ImageModel(name: "photo_19"),
+        ImageModel(name: "photo_20"),
+        ImageModel(name: "photo_21"),
+        ImageModel(name: "photo_22"),
+        
+    ]
+//
+    
+}
